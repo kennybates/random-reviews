d8b7c8dfc03779382952e30e623a015e3c68aa0c
Kenny Bates <kennyleebates@outlook.com>
2021-10-19 15:11:15 -0400

    Update App.js
    updated imports for Review and to test import for react-icon.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import reviews from './data';
+import Review from './Review';
+import { FaGithubSquare } from 'react-icons/fa';
 
 function App() {
   return (
    <div>
      <h2> Reviews </h2>
+     <FaGithubSquare />
+
    </div>
   );
 }
