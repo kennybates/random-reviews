bdf9309fb8e96df47b713b5577b1d323694734c8
Kenny Bates <kennyleebates@outlook.com>
2021-10-19 16:09:54 -0400

    Update Review.js
    Created functions checkNumber, nextPerson, prevPerson, and randomPerson to handle onClick functions for each button.

--- src/Review.js
+++ src/Review.js
@@ -5,6 +5,45 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
     const [index, setIndex] = useState(0); // index state value
     const {name, job, image, text} = people[index]; // access properties from specific person from people[index]
+
+    // checkNumber function - allows number to iterate back
+    const checkNumber = (number) => {
+        if(number > people.length - 1) {
+            return 0;
+        }
+        if (number < 0){
+            return people.length - 1;
+        } else {
+            return number;
+        }
+    };
+
+    // nextPerson function
+    const nextPerson = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            return checkNumber(newIndex);
+        });
+    };
+    // prevPerson function
+    const prevPerson = () => {
+        setIndex(() => {
+            let newIndex = index - 1;
+            return checkNumber(newIndex);
+        });
+    };
+
+    // randomPerson function
+    const randomPerson = () => {
+    
+        let randomIndex = Math.floor(Math.random() * people.length);
+        if (randomIndex === index){ // if statement - not to select current index
+            randomIndex = index + 1;
+        }
+        setIndex(checkNumber(randomIndex));
+        
+    };
+
     return (
         <article className='review'>
             <div className='img-container'>
@@ -17,14 +56,14 @@ const Review = () => {
             <p className='job'>{job}</p>
             <p className='info'>{text}</p>
             <div className='button-container'>
-                <button className='prev-btn'>
+                <button className='prev-btn' onClick={prevPerson}>
                     <FaChevronLeft />
                 </button>
-                <button className='next-btn'>
+                <button className='next-btn' onClick={nextPerson}>
                     <FaChevronRight />
                 </button>
             </div>
-            <button className='random-btn'>
+            <button className='random-btn' onClick={randomPerson}>
                     Random
                 </button>
 
