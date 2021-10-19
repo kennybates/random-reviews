13ee10c62f00aca5550601443ef3d11626d85cdc
Kenny Bates <kennyleebates@outlook.com>
2021-10-19 15:27:48 -0400

    Update Review.js
    Create state values for index and for properties from people array. People array access people[index] to access specific person. Additionally, created structure for return of people.

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import people from './data';
-//import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-    return <h2> Review component</h2>;
+    const [index, setIndex] = useState(0); // index state value
+    const {name, job, image, text} = people[index]; // access properties from specific person from people[index]
+    return (
+        <article className='review'>
+            <div className='img-container'>
+                <img src={image} alt={name} className='person-img'/>
+            </div>
+
+        </article>
+    );
 };
 
 export default Review;
